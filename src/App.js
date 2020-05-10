00ddeae5afcbac22d041ef61beb482dcc28a3163
raqyuste <raquelyustetorregrosa@gmail.com>
2020-05-10 17:43:02 +0200

    starting to apply styles

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { Route,  BrowserRouter } from 'react-router-dom';
 import ConfigurationGame from './components/ConfigurationGame';
 import Home from './components/Home';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPoo, faUserPlus } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
+    library.add(faPoo, faUserPlus);
+
     return (
         <BrowserRouter>
             <Route exact path="/" component={Home} />
